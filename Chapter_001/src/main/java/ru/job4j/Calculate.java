bf0fd10880ee4f90410baa4eeb0f0a72d8fa5e54
Vladislav Sorokin <vlad.sorokin1987@mail.ru>
2017-12-02 12:46:57 +0300

    1.3. JavaDoc. Code convention [#4087]

--- Chapter_001/src/main/java/ru/job4j/Calculate.java
+++ Chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,27 @@
+/**
+ *Calculator
+*/
 package ru.job4j;
+
+/**
+  *Class Calculate решение задачи части 001 урок 1.
+  *@author Sorokin V.D.
+  *@since 02.12.2017
+*/
 
 public class Calculate
 {
+		
 	public static void main (String[] args)
-	{System.out.println ("Hello World");}
+	{Calculate calc = new Calculate();
+	System.out.println(calc.echo ("Hello World"));}
+	
+	public final String echo (String value)
+		{
+		if (value != null) 
+			{value = String.format("%s value=", value);}
+				else {value ="value=null";}
+		return String.format ("%s %s %s", value, value, value);
+		}
 }
+	
---
+++ Chapter_001/src/main/java/ru/job4j/Package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Sorokin V.D. 
+* @version $Id$
+* @since 02.12.2017
+*/
+package ru.job4j;
